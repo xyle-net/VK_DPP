9f7b98780bd71632de8c0ca29e695cc969e0c64f
Nadezhda <nadezhda13soloveva@mail.ru>
2025-03-10 19:34:31 +0300

    Correcr dataset parser for S3 bucket

--- data/parsers/mapping_to_csv/dataset_creator.py
+++ data/parsers/mapping_to_csv/dataset_creator.py
@@ -1,11 +1,22 @@
 import json
 import pandas as pd
-import sys
+import boto3
+from io import StringIO
+
+S3_BUCKET_NAME = 'xyle-net'
+
+s3_client = boto3.client(
+    "s3",
+    endpoint_url="https://s3.cloud.ru",
+    region_name="ru-central-1",
+    aws_access_key_id="<access_key>",
+    aws_secret_access_key="<secret_key>"
+)
 
 
-def flatten_json(json_data, doc_id):
+def flatten_json(json_data: dict, doc_id: str):
     rows = []
-    
+
     def _flatten(element, section_name, part_name=None):
         if isinstance(element, dict):
             for key, value in element.items():
@@ -22,44 +33,53 @@ def flatten_json(json_data, doc_id):
         elif isinstance(element, list):
             for item in element:
                 _flatten(item, section_name, part_name)
-    
+
     for section in json_data:
         for section_name, content in section.items():
             _flatten(content, section_name)
-    
+
     return rows
 
 
-def json_to_table(file_path):
+def json_to_table(json_data: dict, doc_id: str):
     all_data = []
-    doc_id = file_path.split("/")[-1].split(".")[0]
-    
-    with open(file_path, "r", encoding="utf-8") as file:
-        json_data = json.load(file)
-    
     rows = flatten_json(json_data, doc_id)
     all_data.extend(rows)
-    
     df = pd.DataFrame(all_data)
     return df
 
 
-if __name__ == "__main__":
-    #if len(sys.argv) != 2:
-    #    print("Использование: python script.py <путь_к_файлу>")
-    #    sys.exit(1)
-    
-    #file_path = sys.argv[1]
-    files = ['/content/diploma_4.json', '/content/diploma_5.json']
+def process_files_from_s3(input_prefix: str, output_prefix: str):
     all_dfs = []
-    for file in files:
-        df = json_to_table(file)
-        all_dfs.append(df)
-    
-    # Объединяем все DataFrame в один
-    final_df = pd.concat(all_dfs, ignore_index=True)
-    #print(final_df)
 
-    output_csv = "data_mapping.csv"
-    final_df.to_csv(output_csv, index=False, encoding="utf-8")
-    print(f"Датасет сохранен в файл: {output_csv}")
+    response = s3_client.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=input_prefix)
+    if 'Contents' not in response:
+        print(f"Файлы в директории {input_prefix} не найдены.")
+        return
+
+    for obj in response['Contents']:
+        file_key = obj['Key']
+        if not file_key.endswith('.json'):
+            continue
+
+        obj = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key=file_key)
+        json_data = json.loads(obj['Body'].read().decode('utf-8'))
+
+        doc_id = file_key.split("/")[-1].split(".")[0]
+        df = json_to_table(json_data, doc_id)
+        all_dfs.append(df)
+
+    final_df = pd.concat(all_dfs, ignore_index=True)
+
+    csv_buffer = StringIO()
+    final_df.to_csv(csv_buffer, index=False, encoding="utf-8")
+    s3_client.put_object(Bucket=S3_BUCKET_NAME, Key=f"{output_prefix}/mapping_dataset.csv", Body=csv_buffer.getvalue())
+
+    print(f"Датасет сохранен в S3: {output_prefix}/mapping_dataset.csv")
+    # print(final_df)
+
+
+if __name__ == "__main__":
+    input_prefix = "mapped"
+    output_prefix = "data"
+    process_files_from_s3(input_prefix, output_prefix)
