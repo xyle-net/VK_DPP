dd0a7aa05276c815f91c1a1b08b4885385ef0a9c
c0de1NcrazIII <161295655+c0de1NcrazIII@users.noreply.github.com>
2025-03-25 18:43:01 +0300

    create chernovik

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold text-white">
+        СДЕЛАЕМ ТВОЙ ОТЧЕТ
+      </h1>
+    </main>
+  );
+}
