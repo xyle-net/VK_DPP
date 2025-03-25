54ad7fe754e9c08f31a484c9d99254e7acdbb361
Kirya <efremenko.kirill2005@gmail.com>
2025-03-25 22:39:05 +0300

    Roma's code. He's run out of internet

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,26 @@
-import { redirect } from 'next/navigation'
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-    redirect('/register') // Автоматический переход на регистрацию
+    return (
+        <div className="min-h-screen bg-black flex flex-col">
+            {/* Кнопка входа в правом верхнем углу */}
+            <div className="self-end p-4">
+                <Link href="/login">
+                    <Button
+                        variant="ghost"
+                        className="text-white hover:text-white hover:bg-transparent"
+                    >
+                        Войти
+                    </Button>
+                </Link>
+            </div>
+
+            {/* Основное содержимое по центру */}
+            <div className="flex-grow flex flex-col items-center justify-center p-4 space-y-4">
+                <h1 className="text-6xl font-bold text-white">Mbl</h1>
+                <p className="text-2xl text-white">СДЕЛАЕМ ТВОЙ ОТЧЕТ</p>
+            </div>
+        </div>
+    )
 }
